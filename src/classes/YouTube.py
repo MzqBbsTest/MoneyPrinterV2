8b111d0e5a2078e8e9d490a1521927761e33b8f1
mazuqiang <844596330@qq.com>
2024-03-18 00:19:01 +0800

    1

--- src/classes/YouTube.py
+++ src/classes/YouTube.py
@@ -160,8 +160,8 @@ class YouTube:
         if response.status_code == 200:
             response_data = json.loads(response.text)
             print("返回的JSON数据：", response_data)
-            if response_data["data"]["end"] == False:
-                response_data = generate_response_continue(self, response_data["data"])
+            if not response_data["data"]["end"]:
+                response_data = self.generate_response_continue(self, response_data["data"])
             response_data["data"]["content"][0] = data["content"][0]  + response_data["data"]["content"][0]
             return response_data
         else:
